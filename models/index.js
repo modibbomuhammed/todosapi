2d52a58e0d70121acb9e1cdeb5f672fedd199f3c
Muhammed Modibbo <modibbomuhammed@gmail.com>
2020-02-14 11:21:16 +0000

    change to database syntax(promise)

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ var mongoose	= require("mongoose");
 
 mongoose.set('debug', true);
 
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true})
 
-// .then(()=> console.log('connnected to the db'));
+.then(()=> console.log('connnected to the db'))
 
-// .catch(err => console.log(`Failed to connect to the database due to ${err}`));
+.catch(err => console.log(`Failed to connect to the database due to ${err}`));
 
 mongoose.Promise = Promise;
 
